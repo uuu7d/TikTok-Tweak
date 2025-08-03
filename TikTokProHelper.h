bcce9702fa85caf32e71c3ae377a70bedf841756
Raed / Dev-land <uu77dd@gmail.com>
2025-08-03 07:18:18 +0300

    Update TikTokProHelper.h

--- TikTokProHelper.h
+++ TikTokProHelper.h
@@ -1,1 +1,10 @@
+#import <UIKit/UIKit.h>
+#import <Photos/Photos.h>
+
+@interface TikTokProHelper : NSObject
+
++ (void)saveMediaFromURL:(NSURL *)mediaURL withExtension:(NSString *)fileExtension;
++ (void)showAlertWithTitle:(NSString *)title message:(NSString *)message;
+
+@end
 
